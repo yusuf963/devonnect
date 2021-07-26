3636271a0e9f25bf8587d2be0d08237bb1e812db
yusuf963 <mohammad963yusuf@gmail.com>
2021-07-27 00:14:20 +0100

    handel registeration error - check if user exists - add avatar to user by gravatar lib - create new user - hash the password by genSalt/ bcrypt - save user to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {check, validationResult} = require('express-validator');
 const User = require('../../models/User')
 
@@ -17,16 +19,34 @@ router.post(
         check('email', 'Please include a valide Email').isEmail(),
         check('password', 'Please include a valid Password').isLength({min: 6})
     ],
-     (req,res)=> {
+    async (req,res)=> {
         const error = validationResult(req);
         if(!error.isEmpty()){
             return res.status(400).json({error: error.array()});
-        }else if(error.isEmpty()){
-            return res.status(200).json({message: 'User created successfully'});
         }
         // create a new user
     const {name, email, password} = req.body;
-    console.log(req.body);
+    try{
+        let user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({error: 'User already exists'});
+        }
+        const avatar = gravatar.url(email, {s: '200', r: 'pg', d: 'mm'});
+        user = new User({
+            name,
+            email,
+            avatar,
+            password,
+        })
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+
+    }catch(err){
+        return res.status(500).send('Server error', err);
+    }
+    // console.log(req.body);
     res.send('User Route!')
 });
 
